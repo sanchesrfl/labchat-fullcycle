765185d18d47609f4c889296e2526170a42bf189
Icaro <iba1987@gmail.com>
2023-08-17 16:13:10 -0300

    feat(sidebar): mock array de usuarios + ngFor

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  users: { name: string; color: string }[] = [
+    { name: 'Rafael Sanches', color: 'chatlab-pink' },
+    { name: 'Fernanda Linhares', color: 'chatlab-red' },
+    { name: 'Fabiola Pinho', color: 'chatlab-yellow' },
+    { name: 'José Francisco', color: 'chatlab-light-blue' },
+    { name: 'Isaque Scheidt', color: 'chatlab-light-green' },
+    { name: 'Icaro Andrade', color: 'chatlab-purple' },
+  ];
 }
