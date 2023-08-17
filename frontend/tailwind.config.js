ce0bd879a8a24e5aa50882f04c6ef8ff2b33d77f
Icaro <iba1987@gmail.com>
2023-08-17 15:45:15 -0300

    feat(sidebar): adicionar tema no tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "chatlab-dark-blue": "#14162E",
+        "chatlab-blue": "#2E88EF",
+        "chatlab-pink": "#FF60AC",
+        "chatlab-red": "#FF6060",
+        "chatlab-yellow": "#FFDC60",
+        "chatlab-purple": "#A660FF",
+        "chatlab-light-blue": "#60B2FF",
+        "chatlab-light-green": "#60FF9F",
+      },
+    },
   },
   plugins: [],
 };
